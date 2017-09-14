534f415ec10b61d9e1076b20ace3c0e4c1335006
MakeMeCool <sabyasachi2017@yahoo.com>
2017-09-14 14:53:23 +0530

    Lite : Added Device make file

---
+++ board/device.mk
@@ -0,0 +1,9 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := CP8298_I00,Coolpad,COOLPAD
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
